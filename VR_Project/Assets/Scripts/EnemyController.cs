3cf83fcd579bede18fd38b2656b4ea5afac4d23b
YoungRichi <34274756+YoungRichi@users.noreply.github.com>
2019-02-03 01:01:07 -0500

    Update
    map layout.
    AI control (Chase player)

---
+++ VR_Project/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float deathDistance = 0.5f;
+    public float distanceAway;
+    public Transform thisObject;
+    public Transform target;
+    private UnityEngine.AI.NavMeshAgent navComponent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        navComponent = this.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(target.position, transform.position);
+        
+        if(target)
+        {
+            navComponent.SetDestination(target.position);
+        }
+
+        else
+        {
+            if (target = null)
+            {
+                target = this.gameObject.GetComponent<Transform>();
+            }
+            else
+            {
+                target = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+        }
+        if (dist <= deathDistance)
+        {
+            //kill player.
+        }
+    }
+}
