af9deeb740a716ed94c2cfc193e559463ab5459c
YoungRichi <34274756+YoungRichi@users.noreply.github.com>
2019-03-13 04:34:54 -0400

    Wave Control

---
+++ VR_Project/Max Scenes/VR_Project/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public enum SpawnState
+    {
+        Spawning,
+        Waiting,
+        Counting
+    }
+
+
+    //===========================
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public Transform[] spawnPoints;
+
+    public float timeBetweenWaves = 10f;
+    public float waveCountdown;
+
+    private float searchCountdown = 1f;
+
+    private SpawnState state = SpawnState.Counting;
+ 
+    //============================
+
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+    }
+
+    void Update()
+    {
+        if (state == SpawnState.Waiting)
+        {
+            //Check if enemy is alive
+            if (!EnemyIsAlive())
+            {
+                Debug.Log("Enemy = 0!");
+                //Begin New Round
+                WaveCompleted();
+            }
+            else
+            {
+                return;
+            }
+
+
+        }
+
+        if (waveCountdown <= 0)
+        {
+            if (state != SpawnState.Spawning)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    void WaveCompleted()
+    {
+        Debug.Log("Wave Comepleted!");
+
+        state = SpawnState.Counting;
+        waveCountdown = timeBetweenWaves;
+
+        if (nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            Debug.Log("All Waves Completed. LOOPING...");
+            //increase difficulty here.
+        }
+        else
+        {
+            nextWave++;
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if (searchCountdown <= 0f)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+            return true;
+    }
+
+    IEnumerator SpawnWave (Wave _wave)
+    {
+        Debug.Log("Spawning Wave: " + _wave.name);
+        state = SpawnState.Spawning;
+        //SPAWN
+        for (int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds( 1f / _wave.rate);
+        }
+
+        state = SpawnState.Waiting;
+        
+        yield break;
+    }
+
+    void SpawnEnemy ( Transform _enemy)
+    {
+        Debug.Log("Spawning" + _enemy.name);
+        Transform _sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        //Instantiate(_enemy, transform.position, transform.rotation);
+        Instantiate(_enemy, _sp.position, _sp.rotation);
+
+    }
+
+}
