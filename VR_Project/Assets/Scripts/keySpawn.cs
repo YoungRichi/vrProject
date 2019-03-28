bb46bf33f573d6f88bde09023e8987e1b93fefeb
TheLampost <34274706+TheLampost@users.noreply.github.com>
2019-03-28 11:36:39 -0400

    key spawn + keys
    door keys door keys

---
+++ VR_Project/Assets/Scripts/keySpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keySpawn : MonoBehaviour
+{
+
+    public GameObject keyObject;
+    public Transform [] spawnkeyPoints;
+    
+     void Start()
+    {
+        Transform spawnPoint = spawnkeyPoints[Random.Range(0, spawnkeyPoints.Length)];
+        Instantiate(keyObject, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    //    //int spawnKey;
+    //    public GameObject [] key;
+    //    public Transform[] spawnKeyPoints;
+
+    //    void randomSpawnKey(Transform key)
+    //    {
+    //        Transform spawnThekey = spawnKeyPoints[Random.Range(0, spawnKeyPoints.Length)];
+    //        print(spawnKeyPoints);
+    //    }
+
+
+}
