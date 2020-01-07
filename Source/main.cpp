d023c81b7fd5c652ef2c0612639cf6daf00f0247
Xian Zhang <39228340+Xian767-6424@users.noreply.github.com>
2020-01-07 14:48:39 -0500

    splash screen
    splash screen

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,23 +1,55 @@
 #include <SFML/Graphics.hpp>
 
+
+void Show(sf::RenderWindow & renderWindow)
+ {
+  sf::Texture image;
+  if (image.loadFromFile("images/SplashScreen.png") != true)
+  {
+   return;
+  }
+
+ sf::Sprite sprite(image);
+
+  renderWindow.draw(sprite);
+  renderWindow.display();
+
+  sf::Event event;
+   while (true)
+   {
+   while (renderWindow.pollEvent(event)){
+      if (event.type == sf::Event::EventType::KeyPressed || event.type == sf::Event::EventType::MouseButtonPressed)
+	  {
+		 return;
+	  }
+	  else if (event.type == sf::Event::Closed)
+	  {
+		  renderWindow.close();
+	  }
+	  else
+	  {
+		  return;
+	  }
+	
+	}
+}
+}
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
+	sf::RenderWindow window(sf::VideoMode(1024, 768, 32), "SFML works!");
+	sf::CircleShape shape(500.f);
 	shape.setFillColor(sf::Color::Green);
 
 	while (window.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
+		
+		Show(window);
 		window.clear();
 		window.draw(shape);
 		window.display();
+
+		
 	}
 
 	return 0;
