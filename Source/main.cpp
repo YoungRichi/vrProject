f4714c15b38bc415cfbebd036586e66cd74be9e8
Xian Zhang <39228340+Xian767-6424@users.noreply.github.com>
2020-01-07 15:07:01 -0500

    splash screen
    splash screen

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,55 +1,94 @@
 #include <SFML/Graphics.hpp>
 
 
-void Show(sf::RenderWindow & renderWindow)
- {
-  sf::Texture image;
-  if (image.loadFromFile("images/SplashScreen.png") != true)
-  {
-   return;
-  }
-
- sf::Sprite sprite(image);
-
-  renderWindow.draw(sprite);
-  renderWindow.display();
-
-  sf::Event event;
-   while (true)
-   {
-   while (renderWindow.pollEvent(event)){
-      if (event.type == sf::Event::EventType::KeyPressed || event.type == sf::Event::EventType::MouseButtonPressed)
-	  {
-		 return;
-	  }
-	  else if (event.type == sf::Event::Closed)
-	  {
-		  renderWindow.close();
-	  }
-	  else
-	  {
-		  return;
-	  }
-	
-	}
-}
-}
+//void Show(sf::RenderWindow & renderWindow)
+// {
+//  sf::Texture image;
+//  if (image.loadFromFile("images/SplashScreen.png") != true)
+//  {
+//   return;
+//  }
+//
+// sf::Sprite sprite(image);
+//
+//  renderWindow.draw(sprite);
+//  renderWindow.display();
+//
+//  sf::Event event;
+//   while (true)
+//   {
+//   while (renderWindow.pollEvent(event)){
+//      if (event.type == sf::Event::EventType::KeyPressed || event.type == sf::Event::EventType::MouseButtonPressed)
+//	  {
+//		 return;
+//	  }
+//	  else if (event.type == sf::Event::Closed)
+//	  {
+//		  renderWindow.close();
+//	  }
+//	  else
+//	  {
+//		  return;
+//	  }
+//	
+//	}
+//}
+//}
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1024, 768, 32), "SFML works!");
-	sf::CircleShape shape(500.f);
-	shape.setFillColor(sf::Color::Green);
+	// First, I load the logo and create an sprite
+	sf::Texture logo;
 
-	while (window.isOpen())
-	{
-		
-		Show(window);
+	if (!logo.loadFromFile("images/SplashScreen.png")) {
+		exit(1);
+	}
+
+	sf::Sprite sp;
+	sp.setTexture(logo);
+	sp.scale(1, 1); // My logo is quite big in fact (1st google result btw)
+
+	int logoWidth = sp.getGlobalBounds().width;
+	int logoHeight = sp.getGlobalBounds().height;
+
+	// With the logo loaded, I know its size, so i create the window accordingly
+	sf::RenderWindow window(sf::VideoMode(logoWidth, logoHeight), "SFML", sf::Style::None); // <- Important!! Style=None removes title
+
+	// To close splash window by timeout, I just suppose you want something like this, don't you?
+	sf::Clock timer;
+	sf::Time time = timer.restart();
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			// event loop is always needed
+		}
+		// Window closed by time
+		time += timer.restart();
+		if (time >= sf::seconds(2.f)) {
+			window.close();
+		}
+
 		window.clear();
-		window.draw(shape);
+		window.draw(sp);
 		window.display();
+	}
 
-		
+	// Then, reuse the window and do things again
+	window.create(sf::VideoMode(600, 400), "SFML");
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		//window.draw(sp);
+		window.display();
 	}
 
 	return 0;
