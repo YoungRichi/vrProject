d162e49e4e772cd03bc76a31e1dbe94e3f1c7644
Xian767-6424 <837323359@qq.com>
2020-02-11 21:33:45 -0500

    update scene function
    update scene function

--- Source/OrangeEngine/OrangeEngine/Scene1.cpp
+++ Source/OrangeEngine/OrangeEngine/Scene1.cpp
@@ -3,8 +3,9 @@
 #include "ScriptingSystem.h"
 #include "ShapeComponent.h"
 #include "oTransform.h"
-#include "../../OrangeEngine.h"
+
 #include "oRigidBody.h"
+
 
 Scene1::Scene1()
 {
@@ -16,9 +17,9 @@ Scene1::~Scene1()
 {
 }
 
-void Scene1::buildScene()
+void Scene1::buildScene(OrangeEngine* Orange)
 {
-	OrangeEngine* Orange = OrangeEngine::GetInstance();
+	
 
 	sf::Time t1 = sf::seconds(0.1f);
 	float milli = t1.asMilliseconds();
@@ -44,6 +45,7 @@ void Scene1::buildScene()
 
 	Actor* moon = new Actor(new oTransform(), new oRigidBody(Orange->GetPhysics()));
 	moon->AddComponent(new ShapeComponent(10, 6, sf::Color::Blue));
+	moon->AddComponent(new ScriptComponent("Assets/Scripts/ExampleScript.lua"));
 	moon->GetTransform()->Translate(0, 0);
 	moon->GetTransform()->Scale(1, 1);
 	sun->AddChild(moon);
@@ -62,7 +64,6 @@ void Scene1::buildScene()
 
 	Orange->Run();
 	delete(sun);
-
 }
 
 /*void Scene1::update(sf::Time elapsedTime)
--- Source/OrangeEngine/OrangeEngine/Scene1.h
+++ Source/OrangeEngine/OrangeEngine/Scene1.h
@@ -1,14 +1,16 @@
 #pragma once
 #include <SFML/System/Time.hpp>
-
+#include "../../OrangeEngine.h"
 
 
 class Scene1
 {
+
 public:
 	Scene1();
 	~Scene1();
-	void buildScene();
+	void buildScene(OrangeEngine* Orange);
+
 	void update(sf::Time dt);
 };
 
